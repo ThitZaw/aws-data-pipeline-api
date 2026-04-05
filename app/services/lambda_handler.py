24a957a33b5ba082d9dfbc9135898fad22827113
ThitZaw <20728150+ThitZaw@users.noreply.github.com>
2026-04-06 00:36:51 +0800

    modify lambder_handler.py

--- app/services/lambda_handler.py
+++ app/services/lambda_handler.py
@@ -1,4 +1,5 @@
 import json
+import urllib.parse
 from app.services.transform import transform_csv_to_parquet
 
 def lambda_handler(event, context):
@@ -6,8 +7,11 @@ def lambda_handler(event, context):
     record = event["Records"][0]
     
     bucket = record["s3"]["bucket"]["name"]
-    input_key = record["s3"]["object"]["key"]
-
+    raw_key = record["s3"]["object"]["key"]
+    input_key = urllib.parse.unquote_plus(raw_key)
+    print("Bucket:", bucket)
+    print("Raw key:", raw_key)
+    print("Decoded key:", input_key)
     # Only process CSV in raw/
     if not input_key.startswith("raw/") or not input_key.endswith(".csv"):
         print("Skipping file:", input_key)
